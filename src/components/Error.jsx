e8168f69b6b9b0c3ea588f064bc963f7194ee4ba
Ameya02 <65841021+Ameya02@users.noreply.github.com>
2023-01-04 17:23:30 +0530

    Initial commit

---
+++ src/components/Error.jsx
@@ -0,0 +1,12 @@
+
+const Error = () => (
+  <div className="w-full flex justify-center items-center">
+        <h1 className="font-bold text-2xl text-white mt-2">
+          Something went wrong Please Try Agian.
+        </h1>
+
+  </div>
+
+  );
+
+export default Error;
